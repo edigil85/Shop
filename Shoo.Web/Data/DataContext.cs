134bdb87d245e3dd7656fcd16a0ec73947dcb901
Edison Alexander Giraldo Pulgarin <edisongiraldo1990@gmail.com>
2019-02-10 20:49:14 -0500

    Create first librarys

---
+++ Shoo.Web/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Shoo.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Shoo.Web.Data.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public class DataContext: DbContext
+    {
+        public DbSet <Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
