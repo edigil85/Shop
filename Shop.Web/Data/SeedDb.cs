c65bad1dac187415d80576c48c2e764125645abe
Edison Alexander Giraldo Pulgarin <edisongiraldo1990@gmail.com>
2019-02-17 18:05:36 -0500

    Imolementation for user

--- Shop.Web/Data/SeedDb.cs
+++ Shop.Web/Data/SeedDb.cs
@@ -4,31 +4,58 @@
     using System.Linq;
     using System.Threading.Tasks;
     using Entities;
+    using Microsoft.AspNetCore.Identity;
+
     public class SeedDb
     {
         private readonly DataContext context;
+        private readonly UserManager<User> userManager;
         private Random random;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, UserManager<User> userManager)
         {
             this.context = context;
+            this.userManager = userManager;
             this.random = new Random();
         }
+
+
+
 
         public async Task SeedAsync()
         {
             await this.context.Database.EnsureCreatedAsync();
 
+            var user = await this.userManager.FindByEmailAsync("edigil85@hotmail.com");
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = "Edison",
+                    LastName = "Giraldo",
+                    Email = "edigil85@hotmail.com",
+                    UserName = "edigil85@hotmail.com",
+                    PhoneNumber="3014621815"
+                };
+
+                var result = await this.userManager.CreateAsync(user, "123456");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+            }
+
+
             if (!this.context.Products.Any())
             {
-                this.AddProduct("Iphone X");
-                this.AddProduct("Magic Mouse");
-                this.AddProduct("iwatch serie 4");
+                this.AddProduct("Iphone X", user);
+                this.AddProduct("Magic Mouse", user);
+                this.AddProduct("iwatch serie 4", user);
                 await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name)
+        private void AddProduct(string name, User user)
         {
             this.context.Products.Add(new Product
             {
