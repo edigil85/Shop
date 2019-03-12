a93e1941d9fb256c8f641b603e355b08cd726bad
edigil85 <edisongiraldo1990@gmail.com>
2019-03-12 18:50:58 -0500

    creacion de mobile proyect

---
+++ Shop.UIForms/Shop.UIForms/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop.UIForms.Infraestructure
+{
+    using Shop.UIForms.ViewModels;
+    class InstanceLocator
+    {
+        public  MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+
+    }
+}
---
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,58 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Shop.UIForms.ViewModels
+{
+    internal class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        public LoginViewModel()
+        {
+            this.Email = "edigil85@hotmail.com";
+            this.Password = "123456";
+        }
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter and email.",
+                    "Accept"
+                    );
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter and password.",
+                    "Accept"
+                    );
+                return;
+            }
+            if (!this.Email.Equals("edigil85@hotmail.com")|| !this.Password.Equals("123456"))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "User or password wrong.",
+                    "Accept"
+                    );
+            }
+            await Application.Current.MainPage.DisplayAlert(
+                    "OK",
+                    "Fuck Yeah!!!--..",
+                    "Accept"
+                    );
+            return;
+        }
+    }
+}
