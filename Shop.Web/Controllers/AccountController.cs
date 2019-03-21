8fa023090a23905019db654931f8d1db16f57518
edigil85 <edisongiraldo1990@gmail.com>
2019-03-21 10:18:43 -0500

    Complete de web proyect

--- Shop.Web/Controllers/AccountController.cs
+++ Shop.Web/Controllers/AccountController.cs
@@ -8,6 +8,7 @@
     using System.Threading.Tasks;
     using Data.Entities;
     using Helpers;
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
@@ -367,6 +368,77 @@
             return View(model);
         }
 
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Index()
+        {
+            var users = await this.userHelper.GetAllUsersAsync();
+            // Lanbda thanks to Gonzalo Jaimes
+            users.ForEach(async u => u.IsAdmin = await this.userHelper.IsUs­erInRoleAsync(u, "Admin"));
+            //foreach (var user in users)
+            //{
+            //    var myUser = await this.userHelper.GetUserByIdAsync(user.Id);
+            //    if (myUser != null)
+            //    {
+            //        user.IsAdmin = await this.userHelper.IsUserInRoleAsync(myUser, "Admin");
+            //    }
+            //}
+
+            return this.View(users);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> AdminOff(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var user = await this.userHelper.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await this.userHelper.RemoveUserFromRoleAsync(user, "Admin");
+            return this.RedirectToAction(nameof(Index));
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> AdminOn(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var user = await this.userHelper.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await this.userHelper.AddUserToRoleAsync(user, "Admin");
+            return this.RedirectToAction(nameof(Index));
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var user = await this.userHelper.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await this.userHelper.DeleteUserAsync(user);
+            return this.RedirectToAction(nameof(Index));
+        }
 
     }
 }
--- Shop.Web/Data/Entities/User.cs
+++ Shop.Web/Data/Entities/User.cs
@@ -2,18 +2,23 @@
 {
     using Microsoft.AspNetCore.Identity;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class User : IdentityUser
     {
         [MaxLength(50, ErrorMessage = "The field {0} only can contain {1} characters length.")]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [MaxLength(50, ErrorMessage = "The field {0} only can contain {1} characters length.")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Display(Name ="Full Name")]
         public string FullName { get { return $"{this.FirstName}{this.LastName}"; } }
 
+        [Display(Name = "Phone Number")]
+        public override string PhoneNumber { get => base.PhoneNumber; set => base.PhoneNumber = value; }
 
         [MaxLength(100, ErrorMessage = "The field {0} only can contain {1} characters length.")]
         public string Address { get; set; }
@@ -21,6 +26,11 @@
         public int CityId { get; set; }
 
         public City City { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Is Admin?")]
+        public bool IsAdmin { get; set; }
+
 
     }
 }
